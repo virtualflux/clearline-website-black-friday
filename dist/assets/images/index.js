761c505d9e9a67b714851a316d6008d440d7f0c0
unknown <info@virtualflux.africa>
2024-05-21 21:42:37 +0100

    removed package-lock file

---
+++ dist/assets/images/index.js
@@ -0,0 +1,122 @@
+import ClearlineLogo from "./ClearlineLogo.png";
+import HomeBanner from "./homeBanner.png";
+import HomeBanner2 from "./homebanner2.png";
+import HomeBanner3 from "./work.jpg";
+import SectionOneImage from "./SectionOne.png";
+import DoctorSmiling from "./portrait-smiling-doctor.png";
+import AirPeace from "./air-peace.png";
+import CDK from "./cdk.png";
+import CWAY from "./cway.png";
+import Greenlife from "./greenlife.png";
+import Vitafoam from "./vitafoam.png";
+import AppleLogo from "./apple.png";
+import PlayStoreLogo from "./google-play-icon.png";
+import PhonesImage from "./phones.png";
+import AboutBannerImage from "./aboutBanner.png";
+import MissionImage from "./missionImage.png";
+import director1 from "./director1.png";
+import director2 from "./director2.jpeg";
+import AboutBottomBg from "./AboutBottomBg.png";
+import ResourcesImage from "./resource-food.png";
+import ResAlcohol from "./resAlcohol.png";
+import ResChildhood from "./resChildhood.png";
+import ResCovid from "./resCovid.png";
+import ResHandWashing from "./resHandWashing.png";
+import ResHepatitis from "./resHepatitis.png";
+import ResImmune from "./resImmune.png";
+import ResSuperFood from "./resSuperFood.png";
+import ResWorkplace from "./resWorkplace.png";
+import HomeLocation from "./home_loc.png";
+import DummyHospital from "./dummyhospital.png";
+import BlankStar from "./blank-star.png";
+import LocationPointer from "./location.png";
+import Calling from "./calling.png";
+import Ellipse from "./ellipse.png";
+import Healthpriority from "./healthpriority.png";
+import evercare from "./evercare.png";
+import isalu from "./isalu.png";
+import lagoon from "./lagoon.png";
+import lifeline from "./lifeline.png";
+import rjolad from "./rjolad.png";
+import QuotationLeft from "./quotation-mark-left.png";
+import QuotationRight from "./quotation-mark-right.png";
+import EbereNwosu from "./EbereNwosu.png";
+import Amaka from "./Amaka-Nwosisi.jpg";
+import ChukwukaObiora from "./Chukwukaobiora.jpeg";
+import Chukwuka from "./Chukwuka-Ebuka.jpg";
+import chukwudalu from "./chukwudalu-nwosu.jpg";
+import Ituah from "./Pastor-Ituah-Ighodalo.jpeg";
+import YAKASAI from "./PHARM.-AHMED-I.-YAKASAI.jpeg";
+import Shailesh from "./Shailesh-Kumar.jpg";
+import ClearlineWhiteLogo from "./clearline-white-logo.png";
+import OlubukolaOlaoye from "./laoye.jpg";
+import NnekaAnozie from "./NnekaAnozie.jpg"
+import ChidinmaChigbo from "./chidinmachigbo.jpeg"
+import Olubunmi from "./olubunmiokubadejo.jpg"
+import FolasadeDosunmu from "./folasadedosunmu.jpg"
+import PraiseDike from "./Barr.-Praise-Dike.jpg"
+import requestQuote from "./requestQuote.jpg"
+import requestQuote2 from "./requestQuote2.jpg"
+import hypertension from "./hypertension.jpg"
+export {
+  OlubukolaOlaoye,
+  NnekaAnozie,
+  ChidinmaChigbo,
+  Olubunmi,
+  FolasadeDosunmu,
+  PraiseDike,
+  ClearlineLogo,
+  HomeBanner,
+  SectionOneImage,
+  DoctorSmiling,
+  AirPeace,
+  CDK,
+  CWAY,
+  Greenlife,
+  Vitafoam,
+  AppleLogo,
+  PlayStoreLogo,
+  PhonesImage,
+  AboutBannerImage,
+  MissionImage,
+  director1,
+  director2,
+  hypertension,
+  AboutBottomBg,
+  ResourcesImage,
+  ResAlcohol,
+  ResChildhood,
+  ResCovid,
+  ResHandWashing,
+  ResHepatitis,
+  ResImmune,
+  ResSuperFood,
+  ResWorkplace,
+  HomeLocation,
+  DummyHospital,
+  BlankStar,
+  LocationPointer,
+  Calling,
+  Ellipse,
+  Healthpriority,
+  HomeBanner2,
+  HomeBanner3,
+  evercare,
+  isalu,
+  lagoon,
+  lifeline,
+  rjolad,
+  QuotationLeft,
+  QuotationRight,
+  EbereNwosu,
+  ChukwukaObiora,
+  Amaka,
+  Chukwuka,
+  chukwudalu,
+  Ituah,
+  YAKASAI,
+  Shailesh,
+  ClearlineWhiteLogo,
+  requestQuote,
+  requestQuote2
+};
---
+++ dist/assets/svgs/index.js
@@ -0,0 +1,37 @@
+import Menu from "./menu.svg";
+import Phone from "./phone.svg";
+import ArrowRight from "./arrow-right.svg";
+import BlueArrowRight from "./blue-arrow-right.svg";
+import ChatCircle from "./chat-circle.svg";
+import TickCircle from "./tick-circle.svg";
+import GreenTickCircle from "./green-tick-circle.svg";
+import LightTickCircle from "./light-tick-circle.svg";
+import Minus from "./minus.svg";
+import Add from "./add.svg";
+import tripleGreenArrowRight from "./tripleGreenArrowRight.svg";
+import tripleGreenArrowLeft from "./tripleGreenArrowLeft.svg";
+import SearchIcon from "./search-icon.svg";
+import Close from "./close.svg";
+import Star from "./star.svg";
+import GreenArrowLeft from "./green-arrow-right.svg";
+import GreenArrowRight from "./green-arrow-left.svg";
+
+export {
+  Menu,
+  Phone,
+  ArrowRight,
+  ChatCircle,
+  TickCircle,
+  BlueArrowRight,
+  LightTickCircle,
+  Minus,
+  Add,
+  tripleGreenArrowRight,
+  tripleGreenArrowLeft,
+  SearchIcon,
+  Close,
+  Star,
+  GreenTickCircle,
+  GreenArrowLeft,
+  GreenArrowRight,
+};
