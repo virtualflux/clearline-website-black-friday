a8869b0522eb39e010a5e376ad7d5b6b92aa55c9
unknown <info@virtualflux.africa>
2024-05-21 22:30:21 +0100

    reversed commits

--- sanity/env.js
+++
@@ -1,6 +0,0 @@
-export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-05-21'
-
-export const dataset = "fekrq9q9";
-export const projectId = "production";
-export const useCdn = false
--- sanity/schema.js
+++
@@ -1,7 +0,0 @@
-import {blockContent} from './schemaTypes/blockContent'
-import {post} from './schemaTypes/post'
-
-
-export const schema = {
-  types: [post,blockContent],
-}
